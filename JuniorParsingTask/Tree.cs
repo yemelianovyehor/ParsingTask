148248ad3a0cde5ba0930e178014a895f96026a8
NeonCharon <egor.no4@gmail.com>
2022-09-08 14:02:48 +0200

    a bit better?

--- JuniorParsingTask/Tree.cs
+++ JuniorParsingTask/Tree.cs
@@ -12,25 +12,27 @@ public class Tree
         Root = root;
     }
 
+
+    //  Checks wether the tree has a node with given value
+    //  If it does - return true and out the node
+    //  If not - return false and Root
     public bool TryGetNode(string value, out Node outNode)
     {
-        outNode=null;
-        if(this.Root!= null){
-            Stack<Node> nodes = new Stack<Node>();
-            nodes.Push(this.Root);
-            while(nodes.Count>0)
+        outNode = this.Root;
+        Stack<Node> nodes = new Stack<Node>();
+        nodes.Push(this.Root);
+        while(nodes.Count>0)
+        {
+            var node = nodes.Pop();
+            if(node.Value == value)
             {
-                var node = nodes.Pop();
-                if(node.Value==value)
+                outNode = node;
+                return true;
+            }
+            if(node.Children.Count != 0){
+                foreach (var child in node.Children)
                 {
-                    outNode=node;
-                    return true;
-                }
-                if(node.Children.Count!=0){
-                    foreach (var child in node.Children)
-                    {
-                        nodes.Push(child);
-                    }
+                    nodes.Push(child);
                 }
             }
         }
